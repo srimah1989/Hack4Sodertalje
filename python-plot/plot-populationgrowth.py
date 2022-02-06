1415ac036ab40d9432320b40ec28c9bb29e1de2b
Sowmiya Theivaprakasam <srinivasan.mahendran@gmail.com>
2022-02-06 01:01:55 +0100

    dem

--- python-plot/plot-populationgrowth.py
+++ python-plot/plot-populationgrowth.py
@@ -5,16 +5,16 @@ df = pd.read_csv('../data/demographics-data/data2use_in_plot.csv')#, names=heade
 
 fig,ax = plt.subplots()
 
-ax.plot(df.year,df.population,color='red')
+ax.plot(df.year,df.population,'-<',color='red')
 ax.set_xlabel("Year")
 ax.set_ylabel("Total Population", color = 'red')
 
 ax2 = ax.twinx()
-ax2.plot(df.year, df.seniors, color = 'green')
+ax2.plot(df.year, df.seniors, '-o' , color = 'green')
 ax2.set_ylabel("Population >65 years", color = 'green')
 ax2.set_xticklabels(df.year)
 fig.tight_layout()
-fig.savefig('demographics-plot',
+fig.savefig('demographics-plot.jpg',
             format='jpeg',
             dpi=100,
             bbox_inches='tight')
